78eacb6ba83f7aee3d3084a9957fcc265a0e3f64
AdamBrik <adam.brik@edu.esiee.fr>
2024-11-21 10:50:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 
 def ispalindrome(s):
-    for i in range(len(a)//2):
-        if a[i] != a[-i-1]:
+    for i in range(len(s)//2):
+        if s[i] != s[-i-1]:
             return False
     return True
 
